2420510123aca91de9a5fe2b19a8b5c3fbab0824
Azhar Shaik <127967467+zohera27@users.noreply.github.com>
2023-05-28 21:03:11 +0530

    Update mvn.groovy

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,81 +1,26 @@
-@Library('Jenkins_Shared_Library') _
+def mvntest(JAVA_HOME) {
 
-pipeline{
-    
-    agent any
-
-    environment{
-
-        JAVA8_HOME = "${tool 'JAVA8'}"
-        JAVA11_HOME = "${tool 'JAVA11'}"
-        MAVEN_HOME = "${tool 'MAVEN'}"
-    }
-
-    parameters{
-        
-        choice(name: 'action', choices: 'create\ndelete', description: 'Choose Create/Destroy')
-        string(name: 'ImageName', description: "Name of the Docker Build", defaultValue: 'javapp')
-        string(name: 'ImageTag', description: "Tag of the Docker Build", defaultValue: 'v1')
-        string(name: 'DockerHubUser', description: "Dokcer HUB User Account", defaultValue: 'zohera27')
-        
-    }
-
-    stages{
-        
-        stage('Git Checkout') {
-
-         when { expression { params.action == 'create' } }
-
-            steps{
-
-                script{
-
-                    git.gitcheckout(
-                        branch: "main",
-                        url: "https://github.com/zohera27/devops_Java_Project.git"
-                    )
-                }
-            }
-        }
-
-        stage('Unit Test maven') {
-
-         when { expression { params.action == 'create' } }
-        
-            steps{
-
-                script{
-
-                    mvn.mvntest(JAVA8_HOME)
-                }
-            }
-        }
-
-        stage('Integration Test maven') {
-
-         when { expression { params.action == 'create' } }
-
-            steps{
-
-                script{
-
-                    mvn.mvnverify(JAVA8_HOME)
-                }
-            }
-        }  
-
-        stage('Maven Build : maven') {
-
-         when { expression { params.action == 'create' } }
-        
-            steps{
-
-                script{
-
-                    mvn.mvnbuild(JAVA8_HOME)
-                }
-            }
-        }
-
-    }
+    sh "JAVA_HOME=$JAVA_HOME ${MAVEN_HOME}/bin/mvn test"
 }
+
+def mvnverify(JAVA_HOME) {
+
+    sh "JAVA_HOME=$JAVA_HOME ${MAVEN_HOME}/bin/mvn verify -DskipUnitTests"
+}
+
+def mvnbuild(JAVA_HOME){
+
+    sh "JAVA_HOME=$JAVA_HOME ${MAVEN_HOME}/bin/mvn clean install"
+}
+
+/*
+def mvntest(mavenhome, toolchain) {
+    sh "${mavenhome}/mvn -Dmaven.toolchains.file:${toolchain} -Djdk-version=1.8 test"
+}
+def mvnverify(mavenhome, toolchain){
+    sh "${mavenhome}/mvn -Dmaven.toolchains.file:${toolchain} -Djdk-version=1.8 verify -DskipUnitTests"
+}
+def mvnbuild(mavenhome, toolchain){
+    sh "${mavenhome}/mvn -Dmaven.toolchains.file:${toolchain} -Djdk-version=1.8 clean install"
+}
+*/
